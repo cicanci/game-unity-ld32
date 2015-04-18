333fbfb396be334bf8e18e7c4a08b03d93950016
Bruno Cicanci <brunocicanci@gmail.com>
2015-04-18 01:32:55 -0300

    Letters generated by script and controlled by the UI

---
+++ LudumDare32/Assets/Scripts/Letter.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Letter : MonoBehaviour {
+
+	public string LetterValue;
+
+	public void PrintLetter() {
+		LetterManager.Instance.PrintLetter(LetterValue);
+	}
+}
